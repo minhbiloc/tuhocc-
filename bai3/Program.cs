ed596929ae4de1623950352fe8834ad6ddd05667
minhbiloc <nqmminh2k3@gmail.com>
2024-10-24 18:58:23 +0700

    bai tap ngay 24/10/2024

---
+++ bai3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitap3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8; 
+            int x = 1;
+            int y = 2;  
+            float z = x/y; 
+            Console.WriteLine( "giá trị của z là "+ z);
+            float z2 =(float) x/y;
+            Console.WriteLine("giá trị của z2 là : " + z2);
+            byte a = 255;
+            int b = a;
+            Console.WriteLine(b);
+            int k = 40000;
+            byte l = (byte) k; // cố tình éo kiểu
+               Console.WriteLine(l);    
+            Console.ReadKey();
+        }
+    }
+}
---
+++ bai5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 115;
+            Console.WriteLine(" kieu du lieu cua x la {0}" , x.GetType().ToString());
+          
+            var y = "Minh";
+
+            Console.WriteLine(" kieu du lieu cua y la {0}", y.GetType().ToString());
+            
+            var k = 8.8;
+            Console.WriteLine(" kieu du lieu cua k la {0}", k.GetType().ToString());
+            Console.ReadKey();
+        }
+    }
+}
